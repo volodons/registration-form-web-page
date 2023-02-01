77fd1a8a943c24af0114b95262c7c32848adc436
Donets Volodymyr <volodons@gmail.com>
2023-02-01 18:49:01 +0000

    Add script.js of task1_3

---
+++ task1_3/scripts/script.js
@@ -0,0 +1,8 @@
+const salary = document.querySelector("#salary");
+const output = document.querySelector(".salary-output");
+
+output.textContent = salary.value;
+
+salary.addEventListener("input", () => {
+  output.textContent = salary.value;
+});
